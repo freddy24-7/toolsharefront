0e8fe28645401b5ef5b112de31dbfc0c188b3be8
freddy24-7 <freddy.austli@aol.com>
2022-04-21 18:25:21 +0200

    adding frontpage logic

---
+++ src/components/StartingPage/StartingPageContent.js
@@ -0,0 +1,11 @@
+import classes from './StartingPageContent.module.css';
+
+const StartingPageContent = () => {
+    return (
+        <section className={classes.starting}>
+            <h1>Deel je spullen - spar geld!</h1>
+        </section>
+    );
+};
+
+export default StartingPageContent;
