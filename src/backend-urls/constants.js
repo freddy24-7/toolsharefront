be1d0b165d4d406025bef740a2a73971fbf0a40b
freddy24-7 <freddy.austli@aol.com>
2023-01-04 23:09:27 +0100

    adding prod config

--- src/backend-urls/constants.js
+++ src/backend-urls/constants.js
@@ -1,4 +1,7 @@
-export const BASE_API_URL = "http://localhost:8080";
+// Production constant
+export const BASE_API_URL = "https://toolshare-production.up.railway.app";
+// Dev constant
+// export const BASE_API_URL = "http://localhost:8080";
 export const SIGN_UP_URL = BASE_API_URL + '/api/authentication/sign-up';
 export const SIGN_IN_URL = BASE_API_URL + '/api/authentication/sign-in';
 export const PARTICIPANT_URL = BASE_API_URL + '/api/participant/participants';
