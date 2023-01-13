c960ff1f1ecc3505283c517f55293bc9832b8f24
freddy24-7 <freddy.austli@aol.com>
2023-01-13 21:14:07 +0100

    dev properties applied

--- src/backend-urls/constants.js
+++ src/backend-urls/constants.js
@@ -1,7 +1,7 @@
 // Production constant for railway.app
-export const BASE_API_URL = "https://toolshare-production.up.railway.app";
+// export const BASE_API_URL = "https://toolshare-production.up.railway.app";
 //Dev constant
-// export const BASE_API_URL = "http://localhost:8080";
+export const BASE_API_URL = "http://localhost:8080";
 // Other constants
 export const SIGN_UP_URL = BASE_API_URL + '/api/authentication/sign-up';
 export const SIGN_IN_URL = BASE_API_URL + '/api/authentication/sign-in';
